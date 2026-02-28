be95ea656086105c00dd19c4e851b66b433cb492
Danilo Santos <dansansi@hotmail.com>
2026-02-27 22:31:57 -0300

    Feito Service de UserGames.

--- MyGamingListAPI/Services/Interfaces/IUserGameService.cs
+++ MyGamingListAPI/Services/Interfaces/IUserGameService.cs
@@ -4,8 +4,8 @@ namespace MyGamingListAPI.Services.Interfaces
 {
     public interface IUserGameService
     {
-        Task<UserGameResponseDto> AddOrUpdateAsync(string userId, UserGameRequestDto dto);
+        Task<UserGameResponseDto> AddOrUpdateGameOnListAsync(string userId, UserGameRequestDto dto);
         Task<IEnumerable<UserGameResponseDto>> GetAllUserGamesAsync(string userId);
-        Task<bool> RemoveAsync (string userId, string externalId);
+        Task<bool> RemoveAsync (string userId, int externalId);
     }
 }
