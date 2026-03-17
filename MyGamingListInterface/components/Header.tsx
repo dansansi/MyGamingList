dbadbe5a665a71ea78401859ea6788aa67af175d
Danilo Santos <dansansi@hotmail.com>
2026-03-17 17:55:46 -0300

    SearchBar feita e add no Header.

--- MyGamingListInterface/components/Header.tsx
+++ MyGamingListInterface/components/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-
+import SearchBar from "./SearchBar";
 export default function Header() {
   return (
     <header className="w-full bg-zinc-800 px-6 py-4 flex items-center justify-between">
       <span className="text-white font-semibold text-xl">MyGamingList</span>
+      <SearchBar></SearchBar>
       <nav>
         <Link href="/login" className="text-zinc-300 hover:text-white text-sm">
           Entrar
