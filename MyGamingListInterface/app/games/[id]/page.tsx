27960d6d0f141b2592264359e489fad96749b287
Danilo Santos <dansansi@hotmail.com>
2026-03-29 03:21:25 -0300

    Implementada imagem e estilização da pag de game.

--- MyGamingListInterface/app/games/[id]/page.tsx
+++ MyGamingListInterface/app/games/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Game } from "@/types/game";
+import Image from "next/image";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -10,12 +11,24 @@ export default async function GamePage({ params }: Props) {
   const game: Game = await response.json();
 
   return (
-    <main className="max-w-4xl mx-auto px-6 py-12">
-      <h1 className="text-3xl font-semibold text-white">{game.name}</h1>
-      <div
-        className="text-zinc-400 mt-12"
-        dangerouslySetInnerHTML={{ __html: game.description ?? "" }}
-      />
+    <main>
+      <div className="relative w-1/2 mx-auto h-[48vh] mt-4">
+        {game.backgroundImage ? (
+          <Image src={game.backgroundImage} alt={`Foto do jogo ${game.name}`} fill className="object-fill" />
+        ) : (
+          <Image src="/assets/gameNotFound1.jpg" alt="Placeholder de foto não encontrada" fill className="object-fill" />
+        )}
+        <div className="absolute inset-0 bg-gradient-to-t from-zinc-900 via-zinc-900/20 to-transparent" />
+        <div className="absolute bottom-0 left-0 right-0 px-8 pb-5 flex items-end justify-between gap-4">
+          <h1 className="text-3xl pt-4 font-semibold text-white">{game.name}</h1>
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto px-8 py-10">
+        <div
+          className="text-xl text-zinc-400 leading-relaxed prose prose-invert max-w-none"
+          dangerouslySetInnerHTML={{ __html: game.description ?? "" }}
+        />
+      </div>
     </main>
   );
 }
