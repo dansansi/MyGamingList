e4f884852c79f43c76b085b7e85953b3f9ba2e21
Danilo Santos <dansansi@hotmail.com>
2026-04-27 22:31:08 -0300

    Added Head to /health endpoint.

--- MyGamingListAPI/Program.cs
+++ MyGamingListAPI/Program.cs
@@ -199,6 +199,6 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
+app.MapMethods("/health", new[] { "GET", "HEAD" }, () => Results.Ok(new { status = "ok" }));
 
 app.Run();
